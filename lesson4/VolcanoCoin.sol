0346f834ad868a9dba1e088242fd87613f16ef2a
backy <happy.backy@gmail.com>
2022-10-21 19:41:04 -0400

    hw4 update

--- lesson4/VolcanoCoin.sol
+++ lesson4/VolcanoCoin.sol
@@ -1,41 +1,45 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract VolcanoCoin{
+contract VolcanoCoin {
+
+  struct Payment {
+    uint256 transferAmount;
+    address recipient;
+  }
 
   uint256 public totalSupply = 10000;
-  address public owner;
-
-  mapping(address => uint256) private balances;
+  address payable public owner;
+  
+  mapping(address => uint) public balances;
+  mapping(address => Payment) public paymentList;
   event totalSupplyChanged(uint256 totalSupplyWasChanged);
+  event transferOccurred(uint256 amountTransferred, address recipient);
 
-    constructor() {
-      owner = msg.sender;
-      balances[owner] = totalSupply;
-    }
+  constructor() {
+    owner = payable(msg.sender);
+    balances[owner] = totalSupply;
+  }
 
-    modifier isOwner{
-      require(msg.sender == owner);
-      _;
-    }
+  modifier isOwner {
+    require(msg.sender == owner);
+    _;
+  }
 
-    function returnTotalSupply() public view returns(uint256) {
-      return totalSupply;
-    }
+  function returnTotalSupply() public view returns(uint256){
+    return totalSupply;
+  }
+  
+  function changeTotalSupply() public isOwner {
+    totalSupply += 1000; 
+    emit totalSupplyChanged(totalSupply);
+  }
 
-    function changeTotalSupply() public isOwner {
-      totalSupply += 1000;
-      emit totalSupplyChanged(totalSupply);
-    }
-    
-    function balanceOf(address account) public view returns(uint256) {
-      return balances[account];
-    }
-
-    function transfer (uint256 amount, address recipient) public returns(uint256) {
-      balances[msg.sender] = balances[msg.sender] - amount;
-      balances[recipient] = balances[recipient] + amount;
-      return balances[recipient];
-    }
-
+  function transfer(uint amount, address recipient) public {
+    require(msg.sender.balance >= amount);
+    balances[msg.sender] -= amount;
+    balances[recipient] += amount; 
+    paymentList[msg.sender] = Payment(amount, recipient);
+    emit transferOccurred(amount, recipient);
+  }
 }
